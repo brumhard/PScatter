1ab696f998417e4ead3c9db3d09f847527a94d28
sIPeye <fobytobi@gmail.com>
2019-08-14 23:16:14 +0200

    added Error Handling to Import method

--- src/PScatter.Class.ps1
+++ src/PScatter.Class.ps1
@@ -1,5 +1,15 @@
 class PScatter {
-    [void] Import ($Path){
+    [void] Import ($Path) {
+        if (-Not (Test-Path $Path)) {
+            $exception = ([System.IO.FileNotFoundException]::new("Path $Path could not be found"))
+            Write-Error -Exception $exception -Category InvalidArgument -TargetObject $Path
+            return
+        }
+        if (-Not ($Path -match "\S+\.psm1")) {
+            $exception = ([System.ArgumentOutOfRangeException]::new("Path", "$Path is not a valid .psm1 file"))
+            Write-Error -Exception $exception -Category InvalidArgument -TargetObject $Path
+            return
+        }
         Import-Module $Path -Global -Force
     }
 }
